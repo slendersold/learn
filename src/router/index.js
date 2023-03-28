bf412a3c1ad8eb72163d32db9ac61e88cec47a2a
slendersold <33201538+slendersold@users.noreply.github.com>
2023-03-28 21:30:49 +0300

    конец второго урока

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,60 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/home',
+    name: 'home2',
+    meta: {layout: 'main'},
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
   }
 ]
 
